81b109a4777f63ae71fcf61399dae97e50a50c8c
wuzifan <wuzifan0817@gmail.com>
2021-04-02 15:36:45 +0800

    init repo

---
+++ src/main/java/com/rolamd/community/community/controller/GreetingController.java
@@ -0,0 +1,16 @@
+package com.rolamd.community.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(name = "name",defaultValue = "roland",required = false)String name, Model model){
+        model.addAttribute("name",name);
+        return "greeting";
+
+    }
+}
